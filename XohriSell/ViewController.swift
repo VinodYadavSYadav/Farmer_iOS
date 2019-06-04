156f10cf9976e6ae06fe9f34ac5b7f3766a08bef
Rohit Kumar <rohit.kumar@reinventsoft.com>
2019-06-04 10:45:00 +0530

    Initial Commit

---
+++ XohriSell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XohriSell
+//
+//  Created by Apple on 04/06/19.
+//  Copyright © 2019 Renewin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
